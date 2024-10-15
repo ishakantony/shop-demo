42d74c2612156236c448fb52a51cf64068413e68
Ishak Antony Darmawan <ishak.antony@yahoo.com>
2024-10-16 07:14:53 +0800

    feat: expose prometheus metrics from app, add prometheus and grafana

--- apps/products/src/products.module.ts
+++ apps/products/src/products.module.ts
@@ -4,9 +4,15 @@ import { ProductsService } from './products.service';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ProductsRepository } from './products.repository';
 import { Product } from './entities/product.entity';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([Product]), LoggerModule],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([Product]),
+    LoggerModule,
+    PrometheusModule.register(),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService, ProductsRepository],
 })
